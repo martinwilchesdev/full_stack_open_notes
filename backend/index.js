aaaaef89970711ee3114839fc6986e95c95f5e3c
Martin Wilches <martindotdev@gmail.com>
2024-06-14 21:50:25 -0500

    Compilacion del front en la raiz del backend

--- backend/index.js
+++ backend/index.js
@@ -2,24 +2,24 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 
-const notes = [
+let notes = [
     {
-        "id": "dce6",
+        "id": 1,
         "content": "HTML is kwai",
         "important": false
     },
     {
-        "id": "9d39",
+        "id": 2,
         "content": "JS is easy",
         "important": false
     },
     {
-        "id": "4988",
+        "id": 3,
         "content": "CSS is for styles",
         "important": true
     },
     {
-        "id": "891f",
+        "id": 4,
         "content": "React is not a framework",
         "important": false
     }
@@ -30,12 +30,20 @@ app.use(express.json())
 // Es posible permitir solicitudes de otros origenes utilizando el middleware cors de Node
 app.use(cors())
 
-app.get('/', (req, res) => {
-    res.json({ content: 'Node.js' })
-})
+/**
+ * Para que Express muestre contenido estatico, una pagina index.html, JavaScript, etc, se utiliza el middleware integrado de Express llamado static.
+ * Siempre que Express reciba peticiones GET, primero verificara si el directorio `dist` contiene un archivo correspondiente a la direccion de la solicitud, en caso de encontrarlo Express lo devolvera.
+ * Las peticiones realizadas a la direccion `localhost:3001` mostraran el Frontend de React, mientras que las solicitudes GET realizadas a la direccion localhost:3001/api/notes seran manejadas por el backend.
+*/
+app.use(express.static('dist'))
 
 app.get('/api/notes', (req, res) => {
     res.json(notes)
+})
+
+app.get('/api/notes/:id', (req, res) => {
+    singleNote = notes.find(note => note.id === Number(req.params.id))
+    res.json(singleNote)
 })
 
 const PORT = process.env.PORT || 3001
